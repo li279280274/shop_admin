c2c8c0470c43ca9e33c4a37f0b4327da1e3bea89
jepson <jepsonpp@qq.com>
2019-10-28 10:25:24 +0800

    功能

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="login">
-    这是登录组件
+   <!--
+      1. type  类型  颜色
+      2. plain   朴素  简约
+      3. round   圆角  胶囊按钮
+      4. circle  圆 border-radius：50%
+    -->
+   <el-button type="danger">这是按钮</el-button>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
-
+// 只要是vue的插件，如果是模块化的开发模式，必须use一下
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
